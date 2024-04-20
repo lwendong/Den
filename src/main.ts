b152c18fe9f2d6620d8a3c0f706c9668727656b4
L <3335603351@qq.com>
2024-04-20 17:43:05 +0800

    引入quasar

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from "vue";
+import { Quasar } from "quasar";
 import "./styles.css";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const myApp = createApp(App);
+
+myApp.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+});
+
+// Assumes you have a <div id="app"></div> in your index.html
+myApp.mount("#app");
